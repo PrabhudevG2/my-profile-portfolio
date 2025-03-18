980ffbdd9ff7f414bc92ec28195b9d76ab7512ae
PrabhudevG2 <75360911+PrabhudevG2@users.noreply.github.com>
2025-03-18 10:05:03 +0000

    Fresh commit to update AuthorDate

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/my-profile-portfolio/' : './', // Add this line
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    mode === 'development' && componentTagger() // No need for .filter(Boolean)
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
